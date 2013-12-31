039775b6cbfe4b6ad9ac9861ffb8a462e88d42d8
Aishwar Muthuraman <aishwar@gmail.com>
2013-12-31 00:33:59 -0500

    Added: validation for the comparison node

--- test/interpreter/index.js
+++ test/interpreter/index.js
@@ -1,35 +1,41 @@
 
 var assert = require('assert');
 var colors = require('colors');
-var NodeMap = require('../../lib/interpreter/load-node-map');
 var atom = require('../atom');
 var parser = require('../../lib/parser');
+var Interpreter = require('../../lib/interpreter');
 
 describe('Interpreter:', function () {
 
   describe('Nodes:', function () {
-  /*
-    describe('comparison: ', function () {
-      var ComparisonNode = NodeMap['comparison'];
+  
+    function node(str) {
+      var rawRootNode = parser.parse(str);
+      var interpreter = new Interpreter(rawRootNode);
+      var outputNode = interpreter.rootNode.body[0];
+      return outputNode;
+    }
+
+    describe('if: ', function () {
       var PASS = true, FAIL = false;
       
-      function test(items) {
+      function testComparisons(items) {
         items.forEach(function (item) {
-          var comparison = 'if (' + item[0] + ') {}';
-          var ifNode = parser.parse(comparison).body[0];
+          var str = 'if (' + item[0] + ') {}';
+          var ifNode = node(str);
           var expectation = (item[1]) ? assert.doesNotThrow : assert.throws;
           var run = (item[2]) ? it.only : it;
           
-          run ('should ' + (item[1] ? 'pass' : 'fail') + ' validation on the comparison: '
-            + item[0], function () {
+          run ('should ' + (item[1] ? 'pass' : 'fail') + ' validation on the comparison: ' +
+            item[0], function () {
             expectation(function () {
-              ComparisonNode.validate(ifNode.condition);
+              ifNode.validate(ifNode.condition);
             });
           });
         });
       }
       
-      test([
+      testComparisons([
         [ '$a > "hello"'        , PASS ],
         [ '$command.ok != 1'    , PASS ],
         [ 'number("123") > 12'  , PASS ],
@@ -40,30 +46,29 @@ describe('Interpreter:', function () {
       ]);
     });
     
-    */
+    
     describe('"Usage" block: ', function () {
-      var BlockNode = NodeMap['block'];
       /*
       it ('should pass validation when everything is a string in it\'s body', function () {
-        var node = parser.parse([
+        var targetNode = node([
           'Usage {',
           '   "abc"',
           '   "def"',
           '}'
-        ].join('\n')).body[0];
-        assert.doesNotThrow(function () { BlockNode.validate(node); });
+        ].join('\n'));
+        assert.doesNotThrow(function () { targetNode.validate(); });
       });
       
       it ('should fail validation when there is a non-string element in it\'s body', function () {
-        var node = parser.parse([
+        var targetNode = node([
           'Usage {',
           '   mv abc',
           '   "def"',
           '}'
-        ].join('\n')).body[0];
-        assert.throws(function () { BlockNode.validate(node); });
+        ].join('\n'));
+        assert.throws(function () { targetNode.validate(); });
       });
-      */
+*/
     });
     
   });
