8d70b98471fcb93fadeb158b4511bb58dd3a5fd1
Aishwar Muthuraman <aishwar@gmail.com>
2013-12-31 00:13:56 -0500

    Added: forEachChild iterator to the AST node

--- test/interpreter/interpreter-hydrate.js
+++ test/interpreter/interpreter-hydrate.js
@@ -72,5 +72,97 @@ describe('Interpreter:', function () {
       assert.lengthOf(ifBlock.alternate, 1);
     });
   });
+
+  function node(str) {
+    var rawRootNode = parser.parse(str);
+    var interpreter = new Interpreter(rawRootNode);
+    var outputNode = interpreter.rootNode.body[0];
+    return outputNode;
+  }
+
+  function ensureNoChildren(desc, str, exclusive) {
+    ensureChildren(desc + ' contain no children', str, [], exclusive);
+  }
+
+  function ensureChildren(desc, str, expectation, exclusive) {
+    ((exclusive) ? it.only : it) (desc, function () {
+      var targetNode = node(str);
+      var visited = [];
+      targetNode.forEachChild(function (child) {
+        visited.push(child.type);
+      });
+      assert.sameMembers(visited, expectation);
+    });
+  }
+
+  describe('Behavior:', function () {
+    describe('forEachChild: ', function () {
+
+      // Leaf nodes
+      ensureNoChildren('comment', '# comments');
+      ensureNoChildren('command', 'command');
+      ensureNoChildren('special-word', 'exit:bad');
+      ensureNoChildren('empty block', 'Usage {}');
+      ensureNoChildren('variable', '$hello');
+      ensureNoChildren('literal:string', '"hello"');
+      ensureNoChildren('literal:number', '5');
+      ensureNoChildren('log:out', '## log');
+      ensureNoChildren('log:err', '#! error');
+
+      // Non-leaf nodes
+
+      // operation
+      ensureChildren('operation contains the input as a child', 'number("abc")',
+        [ 'literal:string' ]);
+
+      // loop
+      ensureChildren('loop contains the list, value and index as children',
+        'loop (lines("abc") : $a, $b) {}',
+        [ 'operation', 'variable', 'variable' ]);
+
+      // if test 1
+      ensureChildren('if statement with comparison and 2 expressions in body contain all 3 as children',
+        [
+          'if ($a > 1) {',
+          '   ## Print',
+          '   mv abc def',
+          '}'
+        ].join('\n'),
+        [ 'comparison', 'log:out', 'command']);
+
+      // if test 2
+      ensureChildren('if statement with not expression and 2 expressions in body contain all 3 as children',
+        [
+          'if (!$command.ok) {',
+          '   #! Error',
+          '}'
+        ].join('\n'),
+        [ 'not', 'log:err' ]);
+
+      // if-else
+      ensureChildren('if statement with not expression and 2 expressions in body contain all 3 as children',
+        [
+          'if ($command.ok) {',
+          '   ## Done',
+          '} else {',
+          '   #! Error',
+          '}'
+        ].join('\n'),
+        [ 'variable', 'log:out', 'log:err' ]);
+
+      // block
+      ensureChildren('Main block with statements inside contains those statements as children',
+        [
+          'Main {',
+          '   if (!$command.ok) {',
+          '      #! Error',
+          '   }',
+          '   ## Hello welcome',
+          '}'
+        ].join('\n'),
+        [ 'if', 'log:out' ]);
+
+    });
+  });
   
 });
